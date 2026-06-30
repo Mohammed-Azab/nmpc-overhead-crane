19e8d9c5d17dde4738d08b62d7560e90fda6b3e3
Monica2120 <monicalingurovski@gmail.com>
2026-06-30 20:08:12 +0200

    Task 2!

--- functions/discreteF.m
+++ functions/discreteF.m
@@ -1,9 +1,8 @@
-function x = discreteF(x, u, strct)
+function [x, t_nl, x_nl] = discreteF(x, u, strct)
 % DISCRETEF  One-step discrete propagation of the non-linear system.
 %   x = discreteF(x, u, strct) integrates x_dot = f(x,u) over one sample T_s.
 %
-%   [~, x_vec] = ode45(@(t,x)(strct.sys.f(x,u)), [0 strct.T_s], x);
-%   x = x_vec(end,:)';
+  [t_nl, x_nl] = ode45(@(t,x)(strct.sys.f(x,u)), [0 strct.T_s], x);
+  x = x_nl(end,:)';
 
-    % ... your code ...
 end
