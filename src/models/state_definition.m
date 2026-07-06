dd2c1f80c9a2c4faee49bb22ac387c3d2b30668f
mohammed-azab <mo7ammed3zab@outlook.com>
2026-07-06 20:20:12 +0200

    Fix matrix A definition in jacobianSys and state_definition functions

--- src/models/state_definition.m
+++ src/models/state_definition.m
@@ -3,7 +3,7 @@ function x_dot = state_definition(x, u, strct)
 %   x_ = state_definition(x, u, strct) returns the continuous-time
 %   derivative of the crane state vector x = [x_load; v_load; x_crane; v_crane].
 %   strct carries shared parameters (T_s, RBF, predictRBF, ...).
-    A = [0 1 0 0; -3 -0.1 3 0.1; 0 0 0 1; 0 0 0 -2];
+    A = [0 1 0 0; -3 -0.1 3 0.1; 0 0 0 1; 0 0 0 -0.2];
 
     g = @(u) (20/pi())*atan((pi()/20)*u);
     B = [0; 0; 0; g(u)];
