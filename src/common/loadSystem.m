bd044d21365d49e02abfc325c6d8c6ec37b47607
mohammed-azab <mo7ammed3zab@outlook.com>
2026-07-07 00:24:19 +0200

    Refactor loadSystem.m to streamline RBF-related function definitions

--- src/common/loadSystem.m
+++ src/common/loadSystem.m
@@ -1,32 +1,23 @@
-%% loadSystem  
+%% loadSystem
 %   Build the crane system.
-%   Running this script defines the model variables used by every task:
-%     Hsim              number of simulation steps
-%     g, dgdu           slip non-linearity and its derivative
-%     f, h              continuous dynamics x_dot = f(x,u), output y = h(x,u)
-%     jacA, jacB        Jacobians  df/dx (constant A) and df/du (function of u)
-%     stepF             one-step propagation over T_s
-
 
 % make sure the parameters are loaded
 if ~exist('A', 'var') || ~exist('T_s', 'var')
     params;
 end
 
-Hsim = ceil(T_max / T_s);
+Hsim   = ceil(T_max / T_s);
+repeat = rbf_repeat;
+t_lin  = linspace(0, T_s*(Hsim-1), Hsim);
+[positionLoad, steps_size, speedCrane] = simulateCran_multi(T_s, Hsim, repeat);
 
 % slip non-linearity
 if exist('RBF', 'var') && ~isempty(RBF)
-    g    = @(u) predictRBF_craneTime(u, RBF);
-    dgdu = @(u) rbfDeriv(u, RBF);
+    g = @(u) reshape(exp(-(u(:) - RBF.centers(:).').^2 / (2*RBF.sigma^2)) * RBF.weights(:), size(u));
 else
-    g    = g_fallback;
-    dgdu = dgdu_fallback;
+    g = g_fallback;
 end
 
 % model equations
-f    = @(x,u) A*x + [0; 0; 0; g(u)];
-h    = @(x,u) C*x;
-jacA = A;                              % df/dx (independent of x and u)
-jacB = @(u) [0; 0; 0; dgdu(u)];        % df/du
-stepF = @(x,u) stepODE(f, x, u, T_s);  % one-step integration over T_s
+f     = @(x,u) A*x + [0; 0; 0; g(u)];
+stepF = @(x,u) stepODE(f, x, u, T_s);   % one-step integration over T_s
